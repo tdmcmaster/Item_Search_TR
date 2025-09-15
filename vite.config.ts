215cb12689b572a3222c26663f3626c4a6569d35
tdmcmaster <taylor.mcmaster@millenniumgeospatial.com>
2025-09-15 16:27:37 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, Plugin } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { createServer } from "./server";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const disableHMR = process.env.DISABLE_HMR === "1";
+  return {
+  server: {
+    host: "::",
+    port: 8095,
+    strictPort: true,
+    hmr: disableHMR ? false : undefined,
+    fs: {
+      allow: ["./client", "./shared"],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+    },
+  },
+  build: {
+    outDir: "dist/spa",
+  },
+  plugins: [react(), expressPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
+      "@shared": path.resolve(__dirname, "./shared"),
+    },
+  },
+}});
+
+function expressPlugin(): Plugin {
+  return {
+    name: "express-plugin",
+    apply: "serve", // Only apply during development (serve mode)
+    configureServer(server) {
+      const app = createServer();
+
+      // Add Express app as middleware to Vite dev server
+      server.middlewares.use(app);
+    },
+  };
+}
